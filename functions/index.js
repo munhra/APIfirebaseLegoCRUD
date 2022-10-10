f88df5680cb6db2e183dff03f7d69844328966a0
Rafael Munhoz A. Silva <rafael.munhoz@venturus.org.br>
2022-10-10 16:13:10 -0300

    CRUD first version

---
+++ functions/index.js
@@ -0,0 +1,63 @@
+const { initializeApp, applicationDefault, cert } = require('firebase-admin/app');
+const admin = require('firebase-admin');
+const functions = require("firebase-functions");
+const express = require('express');
+const app = express();
+
+admin.initializeApp();
+
+app.get('/:id', async (req, res) => {
+  const id = req.params.id;
+  const legoPartWithId = await getLegoPartById(id);
+  res.status(200).json(legoPartWithId);
+});
+
+app.get('/', async (req, res) => {
+  const allLegoParts = [];
+  const snapShot = await admin.firestore().collection('lego_parts').get();
+  snapShot.forEach(doc => {
+    legoPartWithId = doc.data();
+    legoPartWithId.id = doc.id;
+    allLegoParts.push(legoPartWithId);
+  });
+  res.status(200).json(allLegoParts);
+});
+
+app.post('/', async (req, res) => {
+  const writeResult = await admin.firestore().collection('lego_parts').add(req.body);
+  const legoPartWithId = await getLegoPartById(writeResult.id);
+  res.status(200).json(legoPartWithId);
+});
+
+app.put('/:id', async (req, res) => {
+  const id = req.params.id;
+  const body = req.body;
+  await admin.firestore().collection('lego_parts').doc(id).set(req.body);
+  const legoPartWithId = await getLegoPartById(id);
+  res.status(200).json(legoPartWithId);
+});
+
+app.delete('/:id', async (req, res) => {
+  const id = req.params.id;
+  const legoPartToDelete = await getLegoPartById(id);
+  if (legoPartToDelete == null) {
+    res.sendStatus(410);
+  } else {
+    await admin.firestore().collection('lego_parts').doc(id).delete();
+    res.status(200).json({id: legoPartToDelete.id});
+  }
+});
+
+async function getLegoPartById(id) {
+  const legoPartRef = await admin.firestore().collection('lego_parts').doc(id);
+  const legoPart = await legoPartRef.get();
+  if (legoPart.data() === undefined) {
+    return null;
+  } else {
+    legoPartWithId = legoPart.data();
+    legoPartWithId.id = legoPart.id;
+    return legoPartWithId;
+  }
+}
+
+exports.lego_parts = functions.https.onRequest(app);

ca6f70bb8a637b2d801d7d66c383d87db51279b5
Rafael Munhoz A. Silva <rafael.munhoz@venturus.org.br>
2022-10-12 17:41:53 -0300

    Added cors feature

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,10 @@
 const admin = require("firebase-admin");
 const functions = require("firebase-functions");
+const cors = require("cors");
 const express = require("express");
 const app = express();
 
+app.use(cors());
 admin.initializeApp();
 
 app.get("/:id", async (req, res) => {
